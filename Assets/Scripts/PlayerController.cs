230b17b80953adfdb20332a7ef28f6e15a4f875e
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2022-04-06 16:20:57 -0300

    Estrutura inicial, movimento do Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.2f;
+    private float currentX, currentY;
+    private Rigidbody2D rb2d;
+    private bool canMove = true;
+    public float initialX, initialY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        currentX        = this.transform.position.x;
+        currentY        = this.transform.position.y;
+        //rb2d.velocity   = Vector2.zero;
+
+        if ( canMove )
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb2d.MovePosition(new Vector2(currentX + speed, currentY));
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb2d.MovePosition(new Vector2(currentX, currentY + speed));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb2d.MovePosition(new Vector2(currentX - speed, currentY));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb2d.MovePosition(new Vector2(currentX, currentY - speed));
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canMove = false;
+        rb2d.MovePosition( new Vector2(initialX, initialY) );
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canMove = true;
+    }
+}
